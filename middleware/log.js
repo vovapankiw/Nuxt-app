afdf6bdfc4918f4eb5113c984170e7646ec06390
Vova Pankiv <vovapankiw@gmail.com>
2019-07-10 20:18:37 +0300

    added logger

---
+++ middleware/log.js
@@ -0,0 +1,3 @@
+export default function (context) {
+  console.log('logging from middleware');
+}
--- store/index.js
+++ store/index.js
@@ -1,9 +1,6 @@
 import Vuex from "vuex";
-<<<<<<< HEAD
-=======
 import axios from "axios";
 import Cookie from 'js-cookies';
->>>>>>> 7cbf473fe6b0f93ff6d24771b355820fcf0849d0
 
 const createStore = () => {
   return new Vuex.Store({
@@ -22,27 +19,19 @@ const createStore = () => {
        let postIndex = state.loadedPost.findIndex(post => post.id === editedPost.id);
         state.loadedPost[postIndex] = editedPost;
       },
-<<<<<<< HEAD
-      setToken(state, token) {
-        state.token = token;
-=======
       setToken (state, token) {
         state.token = token;
       },
       clearToken(state) {
         state.token = null;
->>>>>>> 7cbf473fe6b0f93ff6d24771b355820fcf0849d0
       }
     },
     actions: {
       nuxtServerInit(vueContext, context) {
-<<<<<<< HEAD
+
         return context.app.$axios.$get('/posts.json')
         .then(data => {
-=======
-        return context.app.$axios.get(process.env.baseUrl + '/posts.json')
-        .then(res => {
->>>>>>> 7cbf473fe6b0f93ff6d24771b355820fcf0849d0
+
           const PostArray = [];
           for(const key in data) {
             PostArray.push({...data[key], id: key })
@@ -57,25 +46,18 @@ const createStore = () => {
           ...post, 
           updatedDate: new Date()
         }
-<<<<<<< HEAD
+
         return this.$axios.$post('https://nuxt-blog-dc733.firebaseio.com/posts.json.json?auth=' + vueContext.state.token, createdPost)
         .then(data => {
           vueContext.commit('addPost', {...createdPost, id: data.name});
-=======
-        return this.$axios.$post('https://cors-anywhere.herokuapp.com/https://nuxt-blog-dc733.firebaseio.com/posts.json?auth=' + vueContext.state.token, createdPost)
-        .then(res => {
-          vueContext.commit('addPost', {...createdPost, id: res.data.name});
->>>>>>> 7cbf473fe6b0f93ff6d24771b355820fcf0849d0
+
         })
         .catch(e => console.log(e))
       },
       editPost(vueContext, editedPost) {
        return this.$axios.$put('https://nuxt-blog-dc733.firebaseio.com/posts/' + editedPost.id + '.json?auth=' + vueContext.state.token, editedPost)
-<<<<<<< HEAD
         .then(data => {
-=======
-        .then(res => {
->>>>>>> 7cbf473fe6b0f93ff6d24771b355820fcf0849d0
+
           vueContext.commit('editPost', editedPost);
         })
         .catch(e => console.log(e))
@@ -83,26 +65,6 @@ const createStore = () => {
       setPosts(vuexContex, posts) {
         vuexContex.commit('setPosts', posts)
       },
-<<<<<<< HEAD
-      authenticateUser(vueContext, authData) {
-        let authUrl = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=' + process.env.fbAPIKey;
-
-        if(!authData.isLogin) {
-          authUrl = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=' + process.env.fbAPIKey
-        }
-        // we return axios in order to be able add then block where it called and extract info
-        return this.$axios.$post(authUrl,
-        {
-          email: authData.email,
-          password: authData.password,
-          returnSecureToken: true,
-        })
-        .then(result =>{
-          vueContext.commit('setToken', result.idToken);
-        })
-        .catch(e => console.log(e))
-      }
-=======
       authenticateUser(vuexContext, data) {
         let authUrl =
         "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=" +
@@ -163,7 +125,6 @@ const createStore = () => {
         localStorage.removeItem('token');
         localStorage.removeItem('tokenExpiration');
       },
->>>>>>> 7cbf473fe6b0f93ff6d24771b355820fcf0849d0
     },
     getters: {
       loadedPost(state) {
